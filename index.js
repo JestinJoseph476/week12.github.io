1337b735d849eeb99ec6b4070b3bf1b01ac5c022
JestinJoseph476 <vala0025@algonquinlive.com>
2019-07-24 18:13:44 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+//Download the Shake, Rattle and Roll starter files. Inside the zip file you will find the following files: index.html, style.css and index.js. All of the changes will be made to the index.js file. Do NOT change the index.html or style.css files.
+//Retrieve the first box by using its id.
+const firstBox =document.getElementById("shake");
+//Add the classes shake, animated, and infinite to the first box.
+firstBox.classList.add("shake","animated","infinite");
+//Add the text "Shake" to the first box.
+//Retrieve the second box by using the class rattle.
+//Remove the class rattle from the second box.
+//Add the class wobble to the second box.
+//Update the text of the second box to "Wobble".
+//Retrieve the third box (NOTE: You will not be able to use a class).
+//Add the class rollIn to the third box.
+//Add the text "Roll" to the third box.
+//Retrieve the four box using the data-animation attribute.
+//Add a class to the four box with the same value as the data-animation attribute. Do NOT "hard code" the string.
+//Retrieve the five box, then choose your own animation and make the box animate forever (See Animate.css).
+//Once completed, submit the index.js file to Shake, Rattle and Roll assignment on Brightspace.*/
